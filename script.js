89cf5a39a53ad021efce6ec5132c417b23560649
Jorge Morales <jmorales113@gmail.com>
2020-02-10 21:35:10 -0800

    Current Weather Displaying

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const APILink = "https://api.openweathermap.org/data/2.5/weather?q="
 const APIKey = "&appid=21f978b6bfc1044800d6bb1cea63c167"
 var fiveDayForcastDisplay;
 const forecastLink = "https://api.openweathermap.org/data/2.5/forecast?q="
+$("#currentDayBox").hide()
+$("#foreCastDisplay").hide()
 
 $("#citySearch").on("click", function(){
     var city = $("#city").val()
@@ -11,12 +13,26 @@ $("#citySearch").on("click", function(){
     var lon
     
 
-    $.ajax({
-        url : queryURL,
-        method: "GET",
+$.ajax({
+    url: queryURL,
+    method: "GET",
 
     }).then(function(response){
-    console.log(response)
-    })
+    console.log(response);
+    $("#currentDayBox").show()
+    $("#foreCastDisplay").show()
+    $("#currentCity").text(response.name + " " + moment().format('MMMM Do YYYY'))
+    var icon = response.weather[0].icon
+    var iconURL = "http://openweathermap.org/img/w/" + icon + ".png"
+    $("#icon").attr("src", iconURL)
+    var currentTempeature = response.main.temp * (9/5) - 459.67
+    $("#currentTemperature").text(("Temperature: " + currentTempeature.toFixed(1) + " °F"))
+    $("#currentHumidity").text("Humidity: " + response.main.humidity + "%")
+    $("#currentWind").text("Wind Speed: " + response.wind.speed + " MPH")
+    lat = response.coord.lat
+    lon = response.coord.lon
 
+
+
+})
 })
